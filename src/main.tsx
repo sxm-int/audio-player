944dd5f0d044102f1b4602e404e4ded97102e583
Mike Burnett <mike.burnett@siriusxm.com>
2025-10-03 15:36:23 -0400

    put streams in module

--- src/main.tsx
+++ src/main.tsx
@@ -5,47 +5,13 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App';
 import './App.css';
+import { streams } from './mocks/streams';
 
 function installDevFetchShim() {
 	if (!import.meta.env.DEV) return;
 	if ((window as any).__FAKE_API_INSTALLED__) return;
 
 	const realFetch = window.fetch.bind(window);
-
-	const streams = [
-		{
-			title: 'Big Buck Bunny - adaptive qualities',
-			url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-		},
-		{
-			title: 'Big Buck Bunny - 480p only',
-			url: 'https://test-streams.mux.dev/x36xhzz/url_6/193039199_mp4_h264_aac_hq_7.m3u8',
-		},
-		{
-			title: 'ARTE China, ABR',
-			url: 'https://test-streams.mux.dev/test_001/stream.m3u8',
-		},
-		{
-			title: 'Ad-insertion in event stream',
-			url: 'https://test-streams.mux.dev/dai-discontinuity-deltatre/manifest.m3u8',
-		},
-		{
-			title: 'hls.js/issues/666',
-			url: 'https://test-streams.mux.dev/issue666/playlists/cisq0gim60007xzvi505emlxx.m3u8',
-		},
-		{
-			title: 'SAMPLE-AES encrypted',
-			url: 'https://test-streams.mux.dev/bbbAES/playlists/sample_aes/index.m3u8',
-		},
-		{
-			title: 'DK Turntable, PTS shifted by 2.3s',
-			url: 'https://test-streams.mux.dev/pts_shift/master.m3u8',
-		},
-		{
-			title: 'Tears of Steel, HLS with IMSC Captions',
-			url: 'https://test-streams.mux.dev/tos_ismc/main.m3u8',
-		},
-	];
 
 	window.fetch = async (input: RequestInfo | URL, init?: RequestInit) => {
 		const href =
---
+++ src/mocks/streams.ts
@@ -0,0 +1,34 @@
+export const streams = [
+	{
+		title: 'Big Buck Bunny - adaptive qualities',
+		url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
+	},
+	{
+		title: 'Big Buck Bunny - 480p only',
+		url: 'https://test-streams.mux.dev/x36xhzz/url_6/193039199_mp4_h264_aac_hq_7.m3u8',
+	},
+	{
+		title: 'ARTE China, ABR',
+		url: 'https://test-streams.mux.dev/test_001/stream.m3u8',
+	},
+	{
+		title: 'Ad-insertion in event stream',
+		url: 'https://test-streams.mux.dev/dai-discontinuity-deltatre/manifest.m3u8',
+	},
+	{
+		title: 'hls.js/issues/666',
+		url: 'https://test-streams.mux.dev/issue666/playlists/cisq0gim60007xzvi505emlxx.m3u8',
+	},
+	{
+		title: 'SAMPLE-AES encrypted',
+		url: 'https://test-streams.mux.dev/bbbAES/playlists/sample_aes/index.m3u8',
+	},
+	{
+		title: 'DK Turntable, PTS shifted by 2.3s',
+		url: 'https://test-streams.mux.dev/pts_shift/master.m3u8',
+	},
+	{
+		title: 'Tears of Steel, HLS with IMSC Captions',
+		url: 'https://test-streams.mux.dev/tos_ismc/main.m3u8',
+	},
+];
